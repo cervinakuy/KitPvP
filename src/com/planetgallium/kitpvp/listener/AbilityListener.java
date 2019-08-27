9cfcb6ac45d94f3368173cda7bcefa48a18b4178
Cervinakuy <cervinakuy@users.noreply.github.com>
2019-08-26 20:50:27 -0700

    Fixed ability item amount handling

--- src/com/planetgallium/kitpvp/listener/AbilityListener.java
+++ src/com/planetgallium/kitpvp/listener/AbilityListener.java
@@ -3,6 +3,7 @@ package com.planetgallium.kitpvp.listener;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import com.planetgallium.kitpvp.api.PlayerAbilityEvent;
 import com.planetgallium.kitpvp.game.Arena;
@@ -11,7 +12,6 @@ import com.planetgallium.kitpvp.util.Config;
 import com.planetgallium.kitpvp.util.Resources;
 import com.planetgallium.kitpvp.util.Sounds;
 import com.planetgallium.kitpvp.util.Toolkit;
-import com.planetgallium.kitpvp.util.XMaterial;
 
 public class AbilityListener implements Listener {
 	
@@ -32,30 +32,31 @@ public class AbilityListener implements Listener {
 		
 		if (p.hasPermission("kp.ability." + kit.toLowerCase())) {
 			
-			// Message
 			if (resources.getKits(kit).getBoolean("Ability.Message.Enabled")) {
 				p.sendMessage(Config.tr(ability.getMessage()));
 			}
 			
-			// Sound
 			if (resources.getKits(kit).getBoolean("Ability.Sound.Enabled")) {
 				p.playSound(p.getLocation(), Sounds.valueOf(ability.getSoundName()).bukkitSound(), 3, ability.getSoundPitch());
 			}
 			
-			// Potions
 			if (resources.getKits(kit).contains("Ability.Potions")) {
 				for (PotionEffect potions : ability.getPotions()) {
 					p.addPotionEffect(potions);
 				}
 			}
 			
-			// Commands
 			if (resources.getKits(kit).getBoolean("Ability.Commands.Enabled")) {
 				Toolkit.runCommands(resources.getKits(arena.getKits().getKit(p.getName())), "Ability", p);
 			}
 			
-			// Item handling
-			Toolkit.getMainHandItem(p).setAmount(Toolkit.getMainHandItem(p).getAmount() - 1);
+			if (Toolkit.getMainHandItem(p).getAmount() == 1) {
+				ItemStack emptyItem = Toolkit.getMainHandItem(p);
+				emptyItem.setAmount(0);
+				Toolkit.setMainHandItem(p, emptyItem);
+			} else {
+				Toolkit.getMainHandItem(p).setAmount(Toolkit.getMainHandItem(p).getAmount() - 1);
+			}
 			
 		} else {
 			
