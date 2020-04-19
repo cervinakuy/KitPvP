3ea4e14232b2c0b4ceb64869cfccdc1cde37a5d2
cervinakuy <16023430+cervinakuy@users.noreply.github.com>
2020-04-18 20:13:39 -0700

    Added new placeholders %kitpvp_max_xp%, %kitpvp_max_level% (and %max_xp%, %max_level% for scoreboard)

--- src/main/java/com/planetgallium/kitpvp/util/Placeholders.java
+++ src/main/java/com/planetgallium/kitpvp/util/Placeholders.java
@@ -1,16 +1,20 @@
 package com.planetgallium.kitpvp.util;
 
+import com.planetgallium.kitpvp.Game;
 import org.bukkit.entity.Player;
 import com.planetgallium.kitpvp.game.Arena;
 
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 
 public class Placeholders extends PlaceholderExpansion {
-	
+
+	private Game plugin;
 	private Arena arena;
+	private Resources resources;
 	
-	public Placeholders(Arena arena) {
-		this.arena = arena;
+	public Placeholders(Game plugin) {
+		this.arena = plugin.getArena();
+		this.resources = plugin.getResources();
 	}
 	
 	@Override
@@ -19,14 +23,16 @@ public class Placeholders extends PlaceholderExpansion {
 		if (p != null) {
 			
 			switch (identifier) {
-			
-			case "stats_kills": return String.valueOf(arena.getStats().getKills(p.getUniqueId()));
-			case "stats_deaths": return String.valueOf(arena.getStats().getDeaths(p.getUniqueId()));
-			case "stats_kdr": return String.valueOf(arena.getStats().getKDRatio(p.getUniqueId()));
-			case "stats_level": return String.valueOf(arena.getLevels().getLevel(p.getUniqueId()));
-			case "stats_experience": return String.valueOf(arena.getLevels().getExperience(p.getUniqueId()));
-			case "player_killstreak": return String.valueOf(arena.getKillStreaks().getStreak(p.getName()));
-			case "player_kit": return arena.getKits().getKit(p.getName());
+
+				case "stats_kills": return String.valueOf(arena.getStats().getKills(p.getUniqueId()));
+				case "stats_deaths": return String.valueOf(arena.getStats().getDeaths(p.getUniqueId()));
+				case "stats_kdr": return String.valueOf(arena.getStats().getKDRatio(p.getUniqueId()));
+				case "stats_level": return String.valueOf(arena.getLevels().getLevel(p.getUniqueId()));
+				case "stats_experience": return String.valueOf(arena.getLevels().getExperience(p.getUniqueId()));
+				case "player_killstreak": return String.valueOf(arena.getKillStreaks().getStreak(p.getName()));
+				case "player_kit": return arena.getKits().getKit(p.getName());
+				case "max_level": return String.valueOf(resources.getLevels().getInt("Levels.General.Level.Maximum"));
+				case "max_xp": return String.valueOf(resources.getLevels().getInt("Levels.General.Experience.Levelup"));
 			
 			}
 			
