e8e7f7c82724b9fb2d7bd45b1c5494b80e7fa190
cervinakuy <16023430+cervinakuy@users.noreply.github.com>
2020-04-25 21:52:59 -0700

    Fixed kit ability detection

--- src/main/java/com/planetgallium/kitpvp/api/EventListener.java
+++ src/main/java/com/planetgallium/kitpvp/api/EventListener.java
@@ -1,5 +1,6 @@
 package com.planetgallium.kitpvp.api;
 
+import com.planetgallium.kitpvp.util.Config;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -26,27 +27,27 @@ public class EventListener implements Listener {
 	
 	@EventHandler
 	public void onAbility(PlayerInteractEvent e) {
-		
+
 		if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-		
+
 			if (Toolkit.inArena(e.getPlayer())) {
-				
+
 				Player p = e.getPlayer();
 				
 				if (arena.getKits().hasKit(p.getName())) {
-					
-					ItemStack currentItem = Toolkit.getMainHandItem(p);
+
+					ItemStack currentItem = e.getItem();
 					
 					if (currentItem.hasItemMeta() && currentItem.getItemMeta().hasDisplayName()) {
-						
-						if (resources.getKits(arena.getKits().getKit(p.getName())).getString("Ability.Activator.Item") != null) {
-						
+
+						if (resources.getKits(arena.getKits().getKit(p.getName())).contains("Ability.Activator.Item")) {
+
 							if (currentItem.getType() == XMaterial.matchXMaterial(resources.getKits(arena.getKits().getKit(p.getName())).getString("Ability.Activator.Item")).get().parseMaterial()) {
-								
+
 								String kit = arena.getKits().getKit(p.getName());
 								
-								if (currentItem.getItemMeta().getDisplayName().replace("§", "&").equals(resources.getKits(kit).getString("Ability.Activator.Name"))) {
-									
+								if (currentItem.getItemMeta().getDisplayName().equals(resources.getKits(kit).getString("Ability.Activator.Name"))) {
+
 									Ability ability = new Ability(resources.getKits(kit), currentItem.getType());
 									Bukkit.getPluginManager().callEvent(new PlayerAbilityEvent(p, ability));
 									
