fc2cd342668d4f27877d0417a93a26205c330438
cervinakuy <16023430+cervinakuy@users.noreply.github.com>
2020-10-15 20:19:28 -0700

    Added caching with Player Tracker to improve server performance

--- src/main/java/com/planetgallium/kitpvp/listener/TrackerListener.java
+++ src/main/java/com/planetgallium/kitpvp/listener/TrackerListener.java
@@ -1,6 +1,8 @@
 package com.planetgallium.kitpvp.listener;
 
+import com.planetgallium.kitpvp.util.CacheManager;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,16 +12,15 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import com.planetgallium.kitpvp.Game;
-import com.planetgallium.kitpvp.util.Config;
 import com.planetgallium.kitpvp.util.Toolkit;
 import com.planetgallium.kitpvp.util.XMaterial;
 
 public class TrackerListener implements Listener {
 
-	private Game plugin;
+	private FileConfiguration config;
 	
 	public TrackerListener(Game plugin) {
-		this.plugin = plugin;
+		this.config = plugin.getConfig();
 	}
 	
 	@EventHandler
@@ -30,47 +31,60 @@ public class TrackerListener implements Listener {
 		if (Toolkit.inArena(player)) {
 			
 			ItemStack item = player.getInventory().getItem(event.getNewSlot());
-			
-			if (item != null) {
-				
-				if (item.getType() == XMaterial.COMPASS.parseMaterial().get()) {
-					
+
+			if (item != null && item.getType() == XMaterial.COMPASS.parseMaterial().get()) {
+
+				if (!CacheManager.getCompassUsers().contains(player.getName())) {
+
 					new BukkitRunnable() {
-						
+
 						@Override
 						public void run() {
 
-							String[] nearestData = Toolkit.getNearestPlayer(player, plugin.getConfig().getInt("PlayerTracker.TrackBelowY"));
+							if (player != null) {
 
-							if (player.getWorld().getPlayers().size() > 1 && nearestData != null) {
+								String[] nearestData = Toolkit.getNearestPlayer(player, config.getInt("PlayerTracker.TrackBelowY"));
 
-								Player nearestPlayer = Bukkit.getPlayer(nearestData[0]);
-								double nearestDistance = Double.parseDouble(nearestData[1]);
+								if (player.getWorld().getPlayers().size() > 1 && nearestData != null) {
 
-								nearestDistance = Math.round(nearestDistance * 10.0) / 10.0;
+									Player nearestPlayer = Bukkit.getPlayer(nearestData[0]);
+									double nearestDistance = Double.parseDouble(nearestData[1]);
 
-								ItemMeta meta = item.getItemMeta();
-								meta.setDisplayName(Config.getS("PlayerTracker.Message").replace("%nearestplayer%", nearestPlayer.getName()).replace("%distance%", String.valueOf(nearestDistance)));
-								item.setItemMeta(meta);
+									nearestDistance = Math.round(nearestDistance * 10.0) / 10.0;
 
-								player.setCompassTarget(nearestPlayer.getLocation());
-								
+									ItemMeta meta = item.getItemMeta();
+									meta.setDisplayName(Toolkit.translate(config.getString("PlayerTracker.Message"))
+											.replace("%nearestplayer%", nearestPlayer.getName())
+											.replace("%distance%", String.valueOf(nearestDistance)));
+									item.setItemMeta(meta);
+
+									player.setCompassTarget(nearestPlayer.getLocation());
+
+									CacheManager.getCompassUsers().add(player.getName());
+
+								} else {
+
+									ItemMeta meta = item.getItemMeta();
+									meta.setDisplayName(Toolkit.translate(config.getString("PlayerTracker.NoneOnline")));
+									item.setItemMeta(meta);
+
+									cancel();
+
+								}
+
 							} else {
-								
-		                		ItemMeta meta = item.getItemMeta();
-		                		meta.setDisplayName(Config.tr(plugin.getConfig().getString("PlayerTracker.NoneOnline")));
-		                		item.setItemMeta(meta);
-		                		
-		                		cancel();
-								
+
+								CacheManager.getCompassUsers().remove(player.getName());
+								cancel();
+
 							}
-							
+
 						}
-						
-					}.runTaskTimer(plugin, 0L, 20L);
-					
+
+					}.runTaskTimer(Game.getInstance(), 0L, 20L);
+
 				}
-				
+
 			}
 			
 		}
--- src/main/java/com/planetgallium/kitpvp/util/CacheManager.java
+++ src/main/java/com/planetgallium/kitpvp/util/CacheManager.java
@@ -2,16 +2,21 @@ package com.planetgallium.kitpvp.util;
 
 import com.planetgallium.kitpvp.api.Kit;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CacheManager {
 
     private static Map<String, Kit> kitCache = new HashMap<>();;
     private static Map<String, Menu> menuCache = new HashMap<>();
+    private static List<String> compassUsers = new ArrayList<>();
 
     public static Map<String, Kit> getKitCache() { return kitCache; }
 
     public static Map<String, Menu> getMenuCache() { return menuCache; }
+
+    public static List<String> getCompassUsers() { return compassUsers; }
 
 }
