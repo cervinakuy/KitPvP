d7ae1e8cc410a0b1fd8cb44e46ce16a58b9add37
cervinakuy <16023430+cervinakuy@users.noreply.github.com>
2020-11-11 00:28:37 -0800

    Added clear cache functionality

--- src/main/java/com/planetgallium/kitpvp/util/CacheManager.java
+++ src/main/java/com/planetgallium/kitpvp/util/CacheManager.java
@@ -10,13 +10,19 @@ import java.util.Map;
 public class CacheManager {
 
     private static Map<String, Kit> kitCache = new HashMap<>();;
-    private static Map<String, Menu> menuCache = new HashMap<>();
+    private static Map<String, Menu> previewMenuCache = new HashMap<>();
     private static List<String> compassUsers = new ArrayList<>();
 
     public static Map<String, Kit> getKitCache() { return kitCache; }
 
-    public static Map<String, Menu> getMenuCache() { return menuCache; }
+    public static Map<String, Menu> getPreviewMenuCache() { return previewMenuCache; }
 
     public static List<String> getCompassUsers() { return compassUsers; }
+
+    public static void clearCaches() {
+        kitCache.clear();
+        previewMenuCache.clear();
+        compassUsers.clear();
+    }
 
 }
