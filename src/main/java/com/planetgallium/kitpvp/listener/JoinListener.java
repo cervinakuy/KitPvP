80cb0dda6238acd1a97d1cc7821bcaa951cc2194
cervinakuy <16023430+cervinakuy@users.noreply.github.com>
2022-05-11 12:00:52 -0700

    Code reorganization

--- src/main/java/com/planetgallium/kitpvp/listener/JoinListener.java
+++ src/main/java/com/planetgallium/kitpvp/listener/JoinListener.java
@@ -1,9 +1,6 @@
 package com.planetgallium.kitpvp.listener;
 
-import com.planetgallium.database.*;
-import com.planetgallium.database.Record;
 import com.planetgallium.kitpvp.util.Resource;
-import com.planetgallium.kitpvp.util.Resources;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -28,20 +25,21 @@ public class JoinListener implements Listener {
 
 	@EventHandler
 	public void onJoin(PlayerJoinEvent e) {
-
 		Player p = e.getPlayer();
 
 		// Update checker
 		if (plugin.needsUpdate()) {
 			if (p.isOp()) {
-				p.sendMessage(Toolkit.translate("&7[&b&lKIT-PVP&7] &aAn update was found: v" + plugin.getUpdateVersion() + " https://www.spigotmc.org/resources/27107/"));
+				p.sendMessage(Toolkit.translate("&7[&b&lKIT-PVP&7] &aAn update was found: v" +
+						plugin.getUpdateVersion() + " https://www.spigotmc.org/resources/27107/"));
 			}
 		}
 
 		arena.getStats().createPlayer(p);
 
 		if (p.getName().equals("cervinakuy")) {
-			e.setJoinMessage(Toolkit.translate("&7[&b&lKIT-PVP&7] &7The Developer of &bKitPvP &7has joined the server."));
+			e.setJoinMessage(Toolkit.translate("&7[&b&lKIT-PVP&7] &7The Developer of &bKitPvP" +
+					" &7has joined the server."));
 		}
 
 		if (Toolkit.inArena(p)) {
@@ -50,38 +48,24 @@ public class JoinListener implements Listener {
 				p.getInventory().setArmorContents(null);
 			}
 
-			arena.addPlayer(p, config.getBoolean("Arena.ToSpawnOnJoin"), config.getBoolean("Arena.GiveItemsOnJoin"));
+			arena.addPlayer(p, config.getBoolean("Arena.ToSpawnOnJoin"),
+					config.getBoolean("Arena.GiveItemsOnJoin"));
 		}
-
 	}
 	
 	@EventHandler
 	public void onWorldChange(PlayerChangedWorldEvent e) {
-
 		Player p = e.getPlayer();
 
 		if (Toolkit.inArena(p)) {
-
 			if (config.getBoolean("Arena.ClearInventoryOnJoin")) {
 				p.getInventory().clear();
 				p.getInventory().setArmorContents(null);
 			}
 			
-			arena.addPlayer(p, config.getBoolean("Arena.ToSpawnOnJoin"), config.getBoolean("Arena.GiveItemsOnJoin"));
-			
-		} else if (Toolkit.inArena(e.getFrom())) {
-
-			// if they left from the kitpvp arena
-			
-			if (config.getBoolean("Arena.ClearInventoryOnLeave")) {
-				p.getInventory().clear();
-				p.getInventory().setArmorContents(null);
-			}
-			
-			arena.removePlayer(p);
-			
+			arena.addPlayer(p, config.getBoolean("Arena.ToSpawnOnJoin"),
+					config.getBoolean("Arena.GiveItemsOnJoin"));
 		}
-		
 	}
 	
 }
